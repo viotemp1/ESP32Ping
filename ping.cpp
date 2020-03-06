214da50827142ab670c1fd30731d3cb3b89ac84e
Marian Craciunescu <marian-craciunescu@users.noreply.github.com>
2020-03-06 11:35:11 +0200

    Added mem_free to remove

--- ping.cpp
+++ ping.cpp
@@ -129,6 +129,7 @@ static err_t ping_send(int s, ip4_addr_t *addr, int size) {
 
     iecho = (struct icmp_echo_hdr *)mem_malloc((mem_size_t)ping_size);
     if (!iecho) {
+	mem_free(iecho);    
         return ERR_MEM;
     }
 
@@ -141,7 +142,7 @@ static err_t ping_send(int s, ip4_addr_t *addr, int size) {
     if ((err = sendto(s, iecho, ping_size, 0, (struct sockaddr*)&to, sizeof(to)))) {
         transmitted++;
     }
-
+    mem_free(iecho);
     return (err ? ERR_OK : ERR_VAL);
 }
 
